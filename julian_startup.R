d274d80144643569613d1c2113aa6e4556472312
julbautista <julbautista@yahoo.com>
2017-03-05 14:24:01 -0500

    changed scale fills

--- julian_startup.R
+++ julian_startup.R
@@ -27,10 +27,13 @@ jbplot <- theme_grey() +
   )
 
 #set scale colours to default
-jbscale <- scale_colour_manual(values = unlist(jbpal,use.names = F))
+jbcol <- scale_colour_manual(values = unlist(jbpal,use.names = F))  
+jbfill <- scale_fill_manual(values = unlist(jbpal, use.names = F))
+
 
 #create default colours for geoms
 update_geom_defaults("point", list(colour = jbpal$green))
+update_geom_defaults("dotplot", list(colour = jbpal$green, fill = jbpal$green, dotsize = 0.5))
 update_geom_defaults("line", list(colour = jbpal$blue))
 update_geom_defaults("smooth", list(colour = jbpal$blue, fill = jbpal$blue))
 update_geom_defaults("hline", list(colour = jbpal$brown))
@@ -38,7 +41,56 @@ update_geom_defaults("vline", list(colour = jbpal$brown))
 update_geom_defaults("abline", list(colour = jbpal$brown))
 update_geom_defaults("ribbon", list(fill = jbpal$blue, colour = NA))
 update_geom_defaults("errorbar", list(colour = jbpal$green, alpha = 0.5, width = 0.1))
+update_geom_defaults("errorbarh", list(colour = jbpal$green, alpha = 0.5, width = 0.1))
 update_geom_defaults("text", list(colour = jbpal$brown, alpha = 0.15))
+update_geom_defaults("density", list(colour = jbpal$blue, alpha = 0.15))
 
 #create function to paste strings together
 `%+%` <- function(a, b) paste0(a, b)
+
+
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
